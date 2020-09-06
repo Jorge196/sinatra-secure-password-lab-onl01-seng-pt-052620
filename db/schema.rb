a38ff0e38204f39fdff76a7e40862d4a0ff23b50
Jorge Ramirez <joramirez96@gmail.com>
2020-09-06 18:13:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200906175734) do
+ActiveRecord::Schema.define(version: 20200906181308) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password_digest"
   end
 
